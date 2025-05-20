b02339719bf22ecbc06d097b86b738a1c272ea46
faith-ogun <125822160+faith-ogun@users.noreply.github.com>
2025-05-20 20:38:11 +0100

    Create bitcoin.py

---
+++ psets/bitcoin.py
@@ -0,0 +1,34 @@
+import sys
+import json
+import requests
+
+# Error handling - handle these before the correct case scenario
+### No argument handling
+if len(sys.argv) == 1:
+    sys.exit("Missing command-line argument")
+
+### Too many arguments handling
+elif len(sys.argv) > 2:
+    sys.exit("Too many command-line arguments")
+
+try:
+### Argument IS number handling
+    bitcoins = float(sys.argv[1])
+
+except (ValueError):
+    sys.exit("Command-line argument is not a number")
+
+except (requests.RequestException):
+    sys.exit("Invalid request.")
+
+# CoinCap Request
+response = requests.get("https://rest.coincap.io/v3/assets/bitcoin?apiKey=1b22ef32120908493e963ff04a359170d7922aa9cd5e474195a8ac5b0f001c34")
+# dont forget https://
+
+o = response.json()
+
+result = float(o["data"]["priceUsd"])
+
+calc = result * bitcoins
+
+print(f"${calc:,.4f}")
